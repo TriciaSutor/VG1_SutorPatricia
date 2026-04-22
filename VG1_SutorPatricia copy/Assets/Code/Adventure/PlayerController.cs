70ca29c5bcf46b79f67ec9c1612983021dc647e0
TriciaSutor <p.sutor@wustl.edu>
2026-04-21 21:23:54 -0500

    Finished Q10

--- VG1_SutorPatricia copy/Assets/Code/Adventure/PlayerController.cs
+++ VG1_SutorPatricia copy/Assets/Code/Adventure/PlayerController.cs
@@ -2,55 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Adventure
 {
-    // Outlets
-    Rigidbody2D _rigidbody;
-    Animator _animator;
-
-    // Configurations
-    public KeyCode keyUp;
-    public KeyCode keyDown;
-    public KeyCode keyLeft;
-    public KeyCode keyRight;
-    public float moveSpeed;
-
-    // Start is called before the first frame update
-    void Start()
+    public enum Direction
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3
     }
 
-    // Update is called once per frame
-    void Update()
+    public class PlayerController : MonoBehaviour
     {
-        float movementSpeed = _rigidbody.velocity.sqrMagnitude;
-        _animator.SetFloat("speed", movementSpeed);
+        // Outlets
+        Rigidbody2D _rigidbody;
+        Animator _animator;
+        SpriteRenderer _spriteRenderer;
+        public Transform[] attackZones;
 
-        if(movementSpeed > 0.1f) {
-            _animator.SetFloat("movementX", _rigidbody.velocity.x);
-            _animator.SetFloat("movementY", _rigidbody.velocity.y);
+        // Configurations
+        public KeyCode keyUp;
+        public KeyCode keyDown;
+        public KeyCode keyLeft;
+        public KeyCode keyRight;
+        public float moveSpeed;
+        public Sprite[] sprites;
+
+        // State Tracking
+        public Direction facingDirection;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)){
-            _animator.SetTrigger("attack");
+        // Update is called once per frame
+        void Update()
+        {
+            float movementSpeed = _rigidbody.velocity.sqrMagnitude;
+            _animator.SetFloat("speed", movementSpeed);
+
+            if(movementSpeed > 0.1f) {
+                _animator.SetFloat("movementX", _rigidbody.velocity.x);
+                _animator.SetFloat("movementY", _rigidbody.velocity.y);
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space)){
+                _animator.SetTrigger("attack");
+
+                // Convert the enumeration to an index
+                int facingDirectionIndex = (int)facingDirection;
+
+                // Get an attack zone from index
+                Transform attackZone = attackZones[facingDirectionIndex];
+
+                // What objects are within a circle at that attack zone?
+                Collider2D[] hits = Physics2D.OverlapCircleAll(attackZone.position, 0.1f);
+
+                // Handle each hit target
+                foreach(Collider2D hit in hits) {
+                    Breakable breakableObject = hit.GetComponent<Breakable>();
+                    if (breakableObject) {
+                        breakableObject.Break();
+                    }
+                }
+            }
+
+            if (Input.GetKey(keyUp)){
+                _rigidbody.AddForce(Vector2.up * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetKey(keyDown)){
+                _rigidbody.AddForce(Vector2.down * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetKey(keyLeft)){
+                _rigidbody.AddForce(Vector2.left * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetKey(keyRight)){
+                _rigidbody.AddForce(Vector2.right * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
         }
 
-        if (Input.GetKey(keyUp)){
-            _rigidbody.AddForce(Vector2.up * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        }
-
-        if (Input.GetKey(keyDown)){
-            _rigidbody.AddForce(Vector2.down * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        }
-
-        if (Input.GetKey(keyLeft)){
-            _rigidbody.AddForce(Vector2.left * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        }
-
-        if (Input.GetKey(keyRight)){
-            _rigidbody.AddForce(Vector2.right * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        void LateUpdate() {
+            for (int i = 0; i < sprites.Length; i++) {
+                if (_spriteRenderer.sprite == sprites[i]) {
+                    facingDirection = (Direction)i;
+                    break;
+                }
+            }
         }
     }
 }
